7985cce2ff55fa0bade8e20791a3bbb22811796c
Gary Liang <yoyo4cash@gmail.com>
2016-11-23 11:33:25 -0800

    more test spec

--- spec/fivestars_spec.rb
+++ spec/fivestars_spec.rb
@@ -14,16 +14,28 @@ describe 'Login and Memberships' do
 
   context "Login" do 
     # users/auth/verify_pin
-    it ": should able to verify pin using phone:pin" do
+    it ": should able to verify pin using valid phone:pin" do
       response = @client.user_verify_pin(@credentials)
       expect(@client.pin_verified?(response)).to eq true
     end
 
+    it ": should return Error 401 Unauthorized if verify pin using invalid phone:pin" do 
+      @credentials = { phone: '5555541943', pin: '1234999' }
+      response = @client.user_verify_pin(@credentials)
+      expect(response.code).to eq 401
+    end
+
     # users/auth/login
-    it ": should able to authenticate using phone:phone_token" do 
+    it ": should able to authenticate using valid phone:phone_token" do 
       payload = { phone: @credentials[:phone], phone_token: @phone_token }
       response = @client.user_login(payload)
       expect(@client.logged_in?(response)).to eq true
+    end
+
+    it ": should return Error 401 Unauthorized if authenticate using invalid phone:phone_token" do 
+      payload = { phone: @credentials[:phone], phone_token: 'invalid_phone_token_blah' }
+      response = @client.user_login(payload)
+      expect(response.code).to eq 401
     end
   end
 
